e9ad88af8ff5730fc3101e1502ae995ce14cccb9
Sanket Sonvane <2235668+sanketss84@users.noreply.github.com>
2018-04-11 00:10:07 +0530

    small tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div id="app">
+    <h1>Hello World</h1>
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h3>{{ msg }}</h3>
+
   </div>
 </template>
 
@@ -26,6 +14,7 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+    // We can use methods, computed here as well
   }
 }
 </script>
@@ -42,19 +31,5 @@ export default {
 
 h1, h2 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
